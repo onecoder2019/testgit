fa8126e6920974508e0799cf99a44d4b44d3f793
hu <hushiguo@onecoder.com.cn>
2019-05-15 16:49:18 +0800

    hsg-onecoder 提交代码

--- TestGit/src/com/onecoder/test/Test.java
+++ TestGit/src/com/onecoder/test/Test.java
@@ -1,5 +1,11 @@
 package com.onecoder.test;
 
+/**
+* <p>Title: Test</p>  
+* <p>Description: </p>  
+* @author onecoder2019/service@onecoder.com.cn  
+* @date 2019年5月15日
+ */
 public class Test {
 
 	private String userName;
---
+++ TestGit/src/com/onecoder/test/Test2.java
@@ -0,0 +1,13 @@
+package com.onecoder.test;
+
+/**
+* <p>Title: Test2</p>  
+* <p>Description: </p>  
+* @author hsg-onecoder/hushiguo@onecoder.com.cn  
+* @date 2019年5月15日
+ */
+public class Test2 {
+
+	private int count;
+	
+}
