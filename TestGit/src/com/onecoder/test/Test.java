a3ae29b02abe889ef5034b78f9d6c45031faa4e5
hu <hushiguo@onecoder.com.cn>
2019-05-15 16:39:01 +0800

    解决冲突。。。

--- TestGit/src/com/onecoder/test/Test.java
+++ TestGit/src/com/onecoder/test/Test.java
@@ -2,13 +2,7 @@ package com.onecoder.test;
 
 public class Test {
 
-<<<<<<< HEAD
-	
 	private String userName;
 	
-	
-=======
-  Private int age;
-  
->>>>>>> branch 'master' of https://github.com/onecoder2019/testgit.git
+	private int age;
 }
